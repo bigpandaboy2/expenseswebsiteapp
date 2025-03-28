a64b100d958910bd4e37951b2d9c84fc8fd8f790
Aibek Narik <aibek.narik8@gmail.com>
2025-03-28 17:07:11 +0500

    Added username validation script and improve register form styling

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data=json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse(
+                {'username_error': 'Username should only contain alphanumeric characters.'},
+                status=400
+            )
+        
+        if User.objects.filter(username=username).exists():
+            return JsonResponse(
+                {'username_error': 'Choose another username since it is already taken.'},
+                status=409
+            )
+        
+        return JsonResponse({'username_valid': True})
+    
 
 class RegistrationView(View):
     def get(self, request):
