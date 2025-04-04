275bba22f2fbffd8eb708d2fdd7f95ece51158bb
Aibek Narik <aibek.narik8@gmail.com>
2025-04-04 11:55:21 +0500

    CRUD

--- expenses/views.py
+++ expenses/views.py
@@ -1,11 +1,94 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from .models import Category, Expense
+from django.contrib import messages
 
 
 @login_required(login_url='/authentication/login')
 def index(request):
-    return render(request, 'expenses/index.html')
+    categories = Category.objects.all()
+    expenses = Expense.objects.filter(owner=request.user)
+
+    context = {
+        'expenses': expenses
+    }
+    return render(request, 'expenses/index.html', context)
+
 
 @login_required(login_url='/authentication/login')
 def add_expense(request):
-    return render(request, 'expenses/add_expense.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST.get('amount')
+        description = request.POST.get('description')
+        category = request.POST.get('category')
+        date = request.POST.get('expense_date')
+
+        if not amount or not description or not category or not date:
+            messages.error(request, 'All fields are required.')
+            return render(request, 'expenses/add_expense.html', {
+             'categories': categories,
+                'values': request.POST
+            })
+
+        Expense.objects.create(
+            amount=amount,
+            description=description,
+            category=category,
+            date=date,
+            owner=request.user
+        )
+
+        messages.success(request, 'Expense added successfully.')
+        return redirect('expenses')
+    
+
+@login_required(login_url='/authentication/login')
+def expense_edit(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all()
+    context = {
+        'expense': expense,
+        'values': expense,
+        'categories': categories
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/edit-expense.html', context)
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required.')
+            return render(request, 'expenses/edit-expense.html', context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'Description is required.')
+            return render(request, 'expenses/edit-expense.html', context)
+
+        expense.owner = request.user
+        expense.amount = amount
+        expense. date = date
+        expense.category = category
+        expense.description = description
+
+        expense.save()
+        messages.success(request, 'Expense updated  successfully!')
+
+        return redirect('expenses')
+    
+
+def delete_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    expense.delete()
+    messages.success(request, 'Expense removed.')
+    return redirect('expenses')
